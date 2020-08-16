e4e095fcf9359e7c353eb87b345789fbe6740267
mukaddim98 <mukaddim1998@gmail.com>
2020-08-16 04:41:28 -0300

    Added movie table with remove feature

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import Movies from "./components/movies";
 
-function App() {
-  return (
-    <main className="container">
-      <h1>Hello World</h1>
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <main className="container">
+        <Movies />
+      </main>
+    );
+  }
 }
 
 export default App;
