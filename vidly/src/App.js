438b905d6ba8b273643042fbb03fcef2dc4c3f4f
mukaddim98 <mukaddim1998@gmail.com>
2020-08-17 18:29:57 -0300

    Added counters and titles

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import Movies from "./components/movies";
+import Counters from "./components/counters";
 
 class App extends Component {
   render() {
     return (
       <main className="container">
+        <Counters />
+        <br />
+        <br />
         <Movies />
       </main>
     );
---
+++ vidly/src/components/counter.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = { value: this.props.value };
+
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.props.children}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
